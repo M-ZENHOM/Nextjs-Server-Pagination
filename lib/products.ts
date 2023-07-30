fe1ed50d2e4760ecc5e5f96a8c98cb7890b6d396
ZENHOM <you@example.com>
2023-07-30 19:43:51 +0300

    📌

--- lib/products.ts
+++ lib/products.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getProducts = async (limit: number, offset: number, query: string, minPrice: number, maxPrice: number) => {
     try {
-        const res = await axios.get(`${`https://api.escuelajs.co/api/v1/products/?&offset=${offset}&limit=${limit}&title=${query}&price_min=${minPrice}&price_max=${maxPrice}`}`)
+        const res = await axios.get(`${`${process.env.PRODUCTS_API}/?&offset=${offset}&limit=${limit}&title=${query}&price_min=${minPrice}&price_max=${maxPrice}`}`)
         return res.data;
     } catch (error) {
         console.log(error);
